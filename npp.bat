9536a04ad162bc4599de211784a3130806b6ddbd
BoiledPeanuts <76102452+BoiledPeanuts@users.noreply.github.com>
2022-05-02 11:49:00 +0100

    Update npp.bat

--- npp.bat
+++ npp.bat
@@ -1,6 +1,9 @@
+@echo off
+setlocal
+
 mkdir "%temp%\CSHCPS\npp"
 
-curl https://github.com/BoiledPeanuts/CSHCPS/blob/main/npp.zip?raw=true -o "%temp%\CSHCPS\npp\npp.zip"
+curl https://github.com/BoiledPeanuts/CSHCPS/blob/main/npp.zip?raw=true -o "%temp%\CSHCPS\npp\npp.zip" 
 
 powershell -command "Expand-Archive -Force '%temp%\CSHCPS\npp\npp.zip' '%temp%\CSHCPS\npp\'"
 
