6a7964ff63927513784702c9ab32e669d88ee8d0
BoiledPeanuts <76102452+BoiledPeanuts@users.noreply.github.com>
2022-05-02 01:56:45 +0100

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,10 +1,10 @@
 @echo off
 setlocal
 
-
 mkdir "%temp%\CSHCPS"
-
 
 curl https://builds.parsecgaming.com/package/parsec-flat-windows32.zip -o "%temp%\CSHCPS\parsec-flat-windows32.zip"
 
 powershell -command "Expand-Archive -Force '%temp%\CSHCPS\parsec-flat-windows32.zip' '%temp%\CSHCPS\'"
+
+del "%temp%\CSHCPS\parsec-flat-windows32.zip"
